e4ca71b44b1875ba9329dac51210c5bd1b0a03e9
dralmostright <dralmostright@gmail.com>
2022-06-19 21:46:05 +0545

    b2e30ea89d31f491aab1

--- vars/executeSQL.groovy
+++ vars/executeSQL.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 
 def call(Map config = [:]) {
-  sh "sqlplus ${config.username}/${config.password}@${config.dbtns} @${config.script}"
+  sh "sqlplus ${config.username}/${config.password}@${config.dbtns} @../../tempdir/${config.script}"
 }
 
